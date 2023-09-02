9dd487cce29f20839303232ac5fefc5ca3cc731a
minamorl <minamorl@users.noreply.github.com>
2023-09-02 19:27:18 +0900

    wip

--- src/data_transformer.rs
+++ src/data_transformer.rs
@@ -1,15 +1,14 @@
-
-use sled::{Db, IVec};
 use serde::{Serialize, Deserialize};
-use serde_json::{to_vec};
+use std::fmt::Debug;
 
-// Transformation Error Types
+#[derive(Debug)]
 pub enum TransformationError {
-    SerializationFailed,
-    DeserializationFailed,
+    SerializationFailed(serde_json::Error),
+    DeserializationFailed(serde_json::Error),
+    DatabaseError(sled::Error),
+    NotFound(String),
 }
 
-// Generic Field
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Field<T> {
     pub field_type: String,
@@ -22,41 +21,11 @@ impl<T> Field<T> {
     }
 }
 
-// Generic Transformable Data
 #[derive(Serialize, Deserialize)]
 pub struct Transformable<T> {
     pub data: T,
 }
 
-// Data Transformation Trait
-pub trait DataTransformer: Sized {
-    fn to_sled(&self, db: &Db) -> Result<(), TransformationError>;
-    fn from_sled(db: &Db, key: &str) -> Result<Self, TransformationError>;
-}
-
-
-// High-order function for data transformation
-pub fn transform_data<T, F>(data: T, transform_fn: F) -> Result<T, TransformationError>
-where
-    F: Fn(T) -> Result<T, TransformationError>,
-{
-    transform_fn(data)
-}
-
-// Serialization and Deserialization using Sled and Serde
-pub fn save_to_sled<T: Serialize>(db: &Db, key: &str, data: &T) -> Result<(), TransformationError> {
-    let serialized_data = to_vec(data).map_err(|_| TransformationError::SerializationFailed)?;
-    db.insert(key, serialized_data).map_err(|_| TransformationError::SerializationFailed)?;
-    Ok(())
-}
-
-pub fn load_from_sled<T: Deserialize<'static>>(db: &Db, key: &str) -> Result<T, TransformationError> {
-    let stored_data = db.get(key)
-        .map_err(|_| TransformationError::DeserializationFailed)?
-        .ok_or(TransformationError::NotFound(key.to_string()))?;
-
-    let deserialized_data: T = from_slice(stored_data.as_ref())
-        .map_err(|_| TransformationError::DeserializationFailed)?;
-
-    Ok(deserialized_data)
+pub trait Transform<Y> {
+    fn transform(&self) -> Result<Y, TransformationError>;
 }
