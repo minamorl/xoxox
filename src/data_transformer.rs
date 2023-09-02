0138abbc578fbab1ccfc558aed942a8b4fdfc27f
minamorl <minamorl@users.noreply.github.com>
2023-09-02 16:11:47 +0900

    wip

--- src/data_transformer.rs
+++ src/data_transformer.rs
@@ -1,149 +1,62 @@
 
-use sled::{Config, Db, IVec};
-use std::collections::HashMap;
+use sled::{Db, IVec};
 use serde::{Serialize, Deserialize};
-use serde_json::{to_vec, from_slice};
-use std::collections::HashSet;
-#[derive(Clone)]
-pub enum FieldType {
-    StringType,
-    // Add other types as needed
+use serde_json::{to_vec};
+
+// Transformation Error Types
+pub enum TransformationError {
+    SerializationFailed,
+    DeserializationFailed,
 }
 
-#[derive(Clone)]
-pub enum Schema {
-    Simple(FieldType),
-    Complex(HashMap<String, Schema>),
+// Generic Field
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Field<T> {
+    pub field_type: String,
+    pub value: T,
 }
 
-#[derive(Clone)]
-pub struct DataTransformer {
-    from: Schema,
-    to: Schema,
-}
-
-trait Transformable {
-    fn transform(&self, data: HashMap<String, String>) -> Result<HashMap<String, String>, String>;
-}
-
-impl Transformable for DataTransformer {
-    fn transform(&self, data: HashMap<String, String>) -> Result<HashMap<String, String>, String> {
-        Ok(data)
-    }
-}
-impl DataTransformer {
-    pub fn new(from: Schema, to: Schema) -> Self {
-        Self { from, to }
-    }
-
-
-    pub fn get_schema_names(&self, schema: &Schema, prefix: String) -> Vec<String> {
-        let mut keys = Vec::new();
-        match schema {
-            Schema::Simple(_) => keys.push(prefix),
-            Schema::Complex(map) => {
-                for (key, value) in map {
-                    let new_prefix = if prefix.is_empty() {
-                        key.clone()
-                    } else {
-                        format!("{}.{}", prefix, key)
-                    };
-                    keys.extend(self.get_schema_names(value, new_prefix));
-                }
-            }
-        }
-        keys
-    }
-
-    // New method to save transformed data to Sled
-    pub fn save_to_sled(&self, db: &Db, data: HashMap<String, String>) -> sled::Result<()> {
-        let transformed_data = match self.transform(data) {
-            Ok(data) => data,
-            Err(_) => return Err(sled::Error::Unsupported("Transformation failed".into())),
-        };
-        let serialized_data = to_vec(&transformed_data).map_err(|_| sled::Error::Unsupported("Serialization failed".into()))?;
-        db.insert("transformed_data", serialized_data)?;
-        Ok(())
-    }
-
-    // New method to load and deserialize data from Sled
-    pub fn load_from_sled(&self, db: &Db) -> sled::Result<HashMap<String, String>> {
-        let stored_data = db.get("transformed_data")?
-            .ok_or_else(|| sled::Error::CollectionNotFound(IVec::from("transformed_data not found".as_bytes().to_vec())))?;
-        let deserialized_data: HashMap<String, String> = from_slice(&stored_data).map_err(|_| sled::Error::Unsupported("Deserialization failed".into()))?;
-        Ok(deserialized_data)
+impl<T> Field<T> {
+    pub fn new(field_type: String, value: T) -> Self {
+        Self { field_type, value }
     }
 }
 
+// Generic Transformable Data
 #[derive(Serialize, Deserialize)]
-pub struct Data {
-    field1: String,
-    field2: String,
+pub struct Transformable<T> {
+    pub data: T,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use sled::{Config, Db};
-    use std::collections::HashMap;
+// Data Transformation Trait
+pub trait DataTransformer: Sized {
+    fn to_sled(&self, db: &Db) -> Result<(), TransformationError>;
+    fn from_sled(db: &Db, key: &str) -> Result<Self, TransformationError>;
+}
 
-    #[test]
-    fn test_schema_names() {
-        let schema = Schema::Complex(
-            vec![
-                ("field1".to_string(), Schema::Simple(FieldType::StringType)),
-                ("field2".to_string(), Schema::Simple(FieldType::StringType))
-            ]
-            .into_iter()
-            .collect(),
-        );
-        let transformer = DataTransformer::new(schema.clone(), schema.clone());
-        let schema_names = transformer.get_schema_names(&schema, "".to_string());
-        assert_eq!(schema_names.into_iter().collect::<HashSet<_>>(), vec!["field1".to_string(), "field2".to_string()].into_iter().collect::<HashSet<_>>());
-    }
 
-    #[test]
-    fn test_data_transformation() {
-        let schema_from = Schema::Complex(
-            vec![
-                ("field1".to_string(), Schema::Simple(FieldType::StringType)),
-                ("field2".to_string(), Schema::Simple(FieldType::StringType))
-            ]
-            .into_iter()
-            .collect(),
-        );
-        let transformer = DataTransformer::new(schema_from.clone(), schema_from.clone());
+// High-order function for data transformation
+pub fn transform_data<T, F>(data: T, transform_fn: F) -> Result<T, TransformationError>
+where
+    F: Fn(T) -> Result<T, TransformationError>,
+{
+    transform_fn(data)
+}
 
-        let mut data = HashMap::new();
-        data.insert("field1".to_string(), "Data1".to_string());
-        data.insert("field2".to_string(), "Data2".to_string());
+// Serialization and Deserialization using Sled and Serde
+pub fn save_to_sled<T: Serialize>(db: &Db, key: &str, data: &T) -> Result<(), TransformationError> {
+    let serialized_data = to_vec(data).map_err(|_| TransformationError::SerializationFailed)?;
+    db.insert(key, serialized_data).map_err(|_| TransformationError::SerializationFailed)?;
+    Ok(())
+}
 
-        let transformed_data = transformer.transform(data.clone()).unwrap();
-        assert_eq!(transformed_data, data);
-    }
-    #[test]
-    fn test_sled_integration() -> sled::Result<()> {
-        let config = Config::new().temporary(true);
-        let db = config.open()?;
-        
-        let schema_from = Schema::Complex(
-            vec![
-                ("field1".to_string(), Schema::Simple(FieldType::StringType)),
-                ("field2".to_string(), Schema::Simple(FieldType::StringType))
-            ]
-            .into_iter()
-            .collect(),
-        );
-        let transformer = DataTransformer::new(schema_from.clone(), schema_from.clone());
+pub fn load_from_sled<T: Deserialize<'static>>(db: &Db, key: &str) -> Result<T, TransformationError> {
+    let stored_data = db.get(key)
+        .map_err(|_| TransformationError::DeserializationFailed)?
+        .ok_or(TransformationError::NotFound(key.to_string()))?;
 
-        let mut data = HashMap::new();
-        data.insert("field1".to_string(), "Data1".to_string());
-        data.insert("field2".to_string(), "Data2".to_string());
+    let deserialized_data: T = from_slice(stored_data.as_ref())
+        .map_err(|_| TransformationError::DeserializationFailed)?;
 
-        transformer.save_to_sled(&db, data.clone())?;
-        let loaded_data = transformer.load_from_sled(&db)?;
-
-        assert_eq!(loaded_data, data);
-        Ok(())
-    }
+    Ok(deserialized_data)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,30 @@
-use std::collections::HashMap;
 mod data_transformer;
 
-use data_transformer::{DataTransformer, FieldType, Schema};
-use sled::Config;
+use data_transformer::{persist, Field, Transformable, DataTransformer};
+use sled::Db;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Message {
+    sender_id: Field<i32>,
+    text: Field<String>,
+}
 
 fn main() -> sled::Result<()> {
-    // Initialize Sled database
-    let config = Config::new().temporary(true);
-    let db = config.open()?;
+    let db = sled::open("demo_db")?;
 
-    // Define schemas
-    let internal_representation = Schema::Complex(
-        vec![
-            ("field1".to_string(), Schema::Simple(FieldType::StringType)),
-            ("field2".to_string(), Schema::Simple(FieldType::StringType)),
-        ]
-        .into_iter()
-        .collect(),
-    );
-    let presentation = Schema::Complex(
-        vec![("field3".to_string(), Schema::Simple(FieldType::StringType))]
-        .into_iter()
-        .collect(),
-    );
-
-    // Define transformation logic
-    let transform_logic = |data: HashMap<String, String>| -> Result<HashMap<String, String>, String> {
-        let field1 = data.get("field1").ok_or("Missing field1")?;
-        let field2 = data.get("field2").ok_or("Missing field2")?;
-        let combined = format!("{} {}", field1, field2);
-
-        let mut transformed_data = HashMap::new();
-        transformed_data.insert("field3".to_string(), combined);
-
-        Ok(transformed_data)
+    let message = Message {
+        sender_id: Field::new("IntegerField".to_string(), 1),
+        text: Field::new("StringField".to_string(), "Hello, world!".to_string()),
     };
 
-    // Create DataTransformer
-    let transformer = DataTransformer::new(internal_representation, presentation);
+    // Save to sled
+    message.to_sled(&db)?;
 
-    // Sample data to transform
-    let mut sample_data = HashMap::new();
-    sample_data.insert("field1".to_string(), "Data1".to_string());
-    sample_data.insert("field2".to_string(), "Data2".to_string());
+    // Load from sled
+    let loaded_message: Message = Message::from_sled(&db, "message")?;
 
-    // Perform transformation and save to Sled
-    transformer.save_to_sled(&db, sample_data)?;
-
-    // Load transformed data from Sled
-    let loaded_data = transformer.load_from_sled(&db)?;
-
-    println!("Loaded transformed data: {:?}", loaded_data);
+    println!("Loaded Message: {:?}", loaded_message);
 
     Ok(())
 }
